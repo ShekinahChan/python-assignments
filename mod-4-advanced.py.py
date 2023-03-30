285b6b05c960b68df658b24c7dd60041426477ac
ShekinahChan <122718165+ShekinahChan@users.noreply.github.com>
2023-03-31 00:23:08 +0800

    Add files via upload

---
+++ mod-4-advanced.py.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python
+# coding: utf-8
+'''Module 4: Individual Programming Assignment 1
+Parsing Data
+This assignment covers your ability to manipulate data in Python.
+'''
+
+def relationship_status(from_member, to_member, social_graph):
+    '''Relationship Status.
+    15 points.
+    Let us pretend that you are building a new app.
+    Your app supports social media functionality, which means that users can have
+    relationships with other users.
+    There are two guidelines for describing relationships on this social media app:
+    1. Any user can follow any other user.
+    2. If two users follow each other, they are considered friends.
+    This function describes the relationship that two users have with each other.
+    Please see "assignment-4-sample-data.py" for sample data. The social graph
+    will adhere to the same pattern.
+    Parameters
+    ----------
+    from_member: str
+        the subject member
+    to_member: str
+        the object member
+    social_graph: dict
+        the relationship data    
+    Returns
+    -------
+    str
+        "follower" if fromMember follows toMember,
+        "followed by" if fromMember is followed by toMember,
+        "friends" if fromMember and toMember follow each other,
+        "no relationship" if neither fromMember nor toMember follow each other.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+
+def tic_tac_toe(board):
+    '''Tic Tac Toe. 
+    15 points.
+    Tic Tac Toe is a common paper-and-pencil game. 
+    Players must attempt to successfully draw a straight line of their symbol across a grid.
+    The player that does this first is considered the winner.
+    This function evaluates a tic tac toe board and returns the winner.
+    Please see "assignment-4-sample-data.py" for sample data. The board will adhere
+    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
+    have more than one winner. The board will only ever have 2 unique symbols at the same time.
+    Parameters
+    ----------
+    board: list
+        the representation of the tic-tac-toe board as a square list of lists
+    Returns
+    -------
+    str
+        the symbol of the winner or "NO WINNER" if there is no winner
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+def eta(first_stop, second_stop, route_map):
+    '''ETA. 
+    20 points.
+    A shuttle van service is tasked to travel along a predefined circlar route.
+    This route is divided into several legs between stops.
+    The route is one-way only, and it is fully connected to itself.
+    This function returns how long it will take the shuttle to arrive at a stop
+    after leaving another stop.
+    Please see the sample data file in this same folder for sample data. The route map will
+    adhere to the same pattern. The route map may contain more legs and more stops,
+    but it will always be one-way and fully enclosed.
+    Parameters
+    ----------
+    first_stop: str
+        the stop that the shuttle will leave
+    second_stop: str
+        the stop that the shuttle will arrive at
+    route_map: dict
+        the data describing the routes
+    Returns
+    -------
+    int
+        the time it will take the shuttle to travel from first_stop to second_stop
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+# In[8]:
+
+
+def relationship_status(from_member, to_member, social_graph):
+    '''Relationship Status.
+    15 points.
+    Let us pretend that you are building a new app.
+    Your app supports social media functionality, which means that users can have
+    relationships with other users.
+    There are two guidelines for describing relationships on this social media app:
+    1. Any user can follow any other user.
+    2. If two users follow each other, they are considered friends.
+    This function describes the relationship that two users have with each other.
+    Please see "assignment-4-sample-data.py" for sample data. The social graph
+    will adhere to the same pattern.
+    Parameters
+    ----------
+    from_member: str
+        the subject member
+    to_member: str
+        the object member
+    social_graph: dict
+        the relationship data    
+    Returns
+    -------
+    str
+        "follower" if fromMember follows toMember,
+        "followed by" if fromMember is followed by toMember,
+        "friends" if fromMember and toMember follow each other,
+        "no relationship" if neither fromMember nor toMember follow each other.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+from_member = "shekinah"
+to_member = "chan"
+social_graph = {"a":10, "b":20, "c":90}
+
+def relationship_status(from_member, to_member, social_graph):
+    if to_member in social_graph.get(from_member, []):
+        if from_member in social_graph.get(to_member, []):
+            return "friends"
+        else:
+            return "follower"
+    elif from_member in social_graph.get(to_member, []):
+        return "followed by"
+    else:
+        return "no relationship"
+    
+print(relationship_status(from_member, to_member, social_graph))
+
+
+# In[4]:
+
+
+def tic_tac_toe(board):
+    '''Tic Tac Toe. 
+    15 points.
+    Tic Tac Toe is a common paper-and-pencil game. 
+    Players must attempt to successfully draw a straight line of their symbol across a grid.
+    The player that does this first is considered the winner.
+    This function evaluates a tic tac toe board and returns the winner.
+    Please see "assignment-4-sample-data.py" for sample data. The board will adhere
+    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
+    have more than one winner. The board will only ever have 2 unique symbols at the same time.
+    Parameters
+    ----------
+    board: list
+        the representation of the tic-tac-toe board as a square list of lists
+    Returns
+    -------
+    str
+        the symbol of the winner or "NO WINNER" if there is no winner
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+board = ["X", "O", "X", "O", "X", "O"]
+
+def tic_tac_toe(board):
+    for row in board:
+        if len(set(row)) == 1 and row[0] != "":
+            return row[0]
+
+    for col_idx in range(len(board)):
+        col = [board[row_idx][col_idx] for row_idx in range(len(board))]
+        if len(set(col)) == 1 and col[0] != "":
+            return col[0]
+
+    diag1 = [board[i][i] for i in range(len(board))]
+    if len(set(diag1)) == 1 and diag1[0] != "":
+        return diag1[0]
+
+    diag2 = [board[i][len(board)-1-i] for i in range(len(board))]
+    if len(set(diag2)) == 1 and diag2[0] != "":
+        return diag2[0]
+
+    return "NO WINNER"
+
+print(tic_tac_toe(board))
+
+
+# In[2]:
+
+
+def eta(first_stop, second_stop, route_map):
+    '''ETA. 
+    20 points.
+    A shuttle van service is tasked to travel along a predefined circlar route.
+    This route is divided into several legs between stops.
+    The route is one-way only, and it is fully connected to itself.
+    This function returns how long it will take the shuttle to arrive at a stop
+    after leaving another stop.
+    Please see the sample data file in this same folder for sample data. The route map will
+    adhere to the same pattern. The route map may contain more legs and more stops,
+    but it will always be one-way and fully enclosed.
+    Parameters
+    ----------
+    first_stop: str
+        the stop that the shuttle will leave
+    second_stop: str
+        the stop that the shuttle will arrive at
+    route_map: dict
+        the data describing the routes
+    Returns
+    -------
+    int
+        the time it will take the shuttle to travel from first_stop to second_stop
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+first_stop = "ateneo"
+second_stop = "regis"
+route_map = {
+    "ateneo": [("miriam", 5), ("UP", 8)],
+    "miriam": [("UP", 6), ("regis", 10)],
+    "UP": [("regis", 7)],
+    "regis": [("ateneo", 12)]
+}
+
+def eta(first_stop, second_stop, route_map):
+    total_time = 0
+        
+    current_stop = first_stop
+    while current_stop != second_stop:
+        for leg in route_map[current_stop]:
+            if leg[0] == second_stop:
+                total_time += leg[1]
+                return total_time
+            else:
+                total_time += leg[1]
+                current_stop = leg[0]
+    
+    return total_time
+
+print(eta(first_stop, second_stop, route_map))
+
+
+# In[ ]:
+
+
+
+
---
+++ mod-4-basic.py.py
@@ -0,0 +1,361 @@
+#!/usr/bin/env python
+# coding: utf-8
+'''Module 2: Individual Programming Assignment 1
+
+Useful Business Calculations
+
+This assignment covers your basic proficiency with Python.
+'''
+
+def savings(gross_pay, tax_rate, expenses):
+    '''Savings.
+    2 points.
+
+    This function calculates the money remaining
+        for an employee after taxes and expenses.
+    
+    To get the take-home pay of an employee, we will
+        follow the following process:
+        1. Apply the tax rate to the gross pay of the employee; round down
+        2. Subtract the expenses from the after-tax pay of the employee
+
+    Parameters
+    ----------
+    gross_pay: int
+        the gross pay of an employee for a certain time period, expressed in centavos
+    tax_rate: float
+        the tax rate for a certain time period, expressed as a number between 0 and 1 (e.g., 0.12)
+    expenses: int
+        the expenses of an employee for a certain time period, expressed in centavos
+
+    Returns
+    -------
+    int
+        the number of centavos remaining from an employee's pay after taxes and expenses
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    '''Material Waste.
+    2 points.
+
+    This function calculates how much material input will be wasted
+        after running a certain number of jobs that consume
+        a set amount of material.
+
+    To get the waste of a set of jobs:
+        1. Multiply the number of jobs by the material consumption per job.
+        2. Subtract the total material consumed from the total material available.
+
+    The users of this function also want you to format the output as a string, annotated with the
+        units in which the material is expressed. Do not add a space between the number and the unit.
+
+    Parameters
+    ----------
+    total_material: int
+        the total material available
+    material_units: str
+        the units used to express a quantity of the material (e.g., "kg", "L", etc.)
+    num_jobs: int
+        the number of jobs to run
+    job_consumption: int
+        the amount of material consumed per job
+
+    Returns
+    -------
+    str
+        the amount of remaining material expressed with its unit (e.g., "10kg").
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+def interest(principal, rate, periods):
+    '''Interest.
+    3 points.
+
+    This function calculates the final value of an investment after
+        gaining simple interest over a number of periods.
+
+    To calculate simple interest, simply multiply the principal to the quantity (rate * time). 
+        Add this amount to the principal to get the final value.
+
+    Round down the final amount.
+
+    Parameters
+    ----------
+    principal: int
+        the principal (i.e., starting) amount invested, expressed in centavos
+    rate: float
+        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)
+    periods: int
+        the number of periods invested
+
+    Returns
+    -------
+    int
+        the final value of the investment
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+def body_mass_index(weight, height):
+    '''Body Mass Index.
+    3 points.
+
+    This function calculates the body mass index (BMI) of a person
+        given their weight and height.
+
+    The formula for BMI is: kg / (m ^ 2)
+        (i.e., kilograms over meters squared)
+
+    Unfortunately, the users of this function use the imperial system.
+        You will need to first convert their arguments to the metric system.
+    
+    Parameters
+    ----------
+    weight: float
+        the weight of the person, in pounds
+    height: list
+        the height of the person, expressed as a list of two integers.
+        the first integer is the foot component of their height.
+        the second integer is the inches component of their height.
+        for example, 5'10" would be passed as [5, 10].
+
+    Returns
+    -------
+    float
+        the BMI of the person.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+# In[3]:
+
+
+def savings(gross_pay, tax_rate, expenses):
+    '''Savings.
+    2 points.
+
+    This function calculates the money remaining
+        for an employee after taxes and expenses.
+    
+    To get the take-home pay of an employee, we will
+        follow the following process:
+        1. Apply the tax rate to the gross pay of the employee; round down
+        2. Subtract the expenses from the after-tax pay of the employee
+
+    Parameters
+    ----------
+    gross_pay: int
+        the gross pay of an employee for a certain time period, expressed in centavos
+    tax_rate: float
+        the tax rate for a certain time period, expressed as a number between 0 and 1 (e.g., 0.12)
+    expenses: int
+        the expenses of an employee for a certain time period, expressed in centavos
+
+    Returns
+    -------
+    int
+        the number of centavos remaining from an employee's pay after taxes and expenses
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+gross_pay = 50000
+tax_rate = 0.22
+expenses = 13000
+
+A = gross_pay - (gross_pay * tax_rate)
+B = A - expenses
+
+take_home_pay = int(B)
+
+print("the number of centavos remaining from an employee's pay after taxes and expenses is", take_home_pay)
+
+
+# In[4]:
+
+
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    '''Material Waste.
+    2 points.
+
+    This function calculates how much material input will be wasted
+        after running a certain number of jobs that consume
+        a set amount of material.
+
+    To get the waste of a set of jobs:
+        1. Multiply the number of jobs by the material consumption per job.
+        2. Subtract the total material consumed from the total material available.
+
+    The users of this function also want you to format the output as a string, annotated with the
+        units in which the material is expressed. Do not add a space between the number and the unit.
+
+    Parameters
+    ----------
+    total_material: int
+        the total material available
+    material_units: str
+        the units used to express a quantity of the material (e.g., "kg", "L", etc.)
+    num_jobs: int
+        the number of jobs to run
+    job_consumption: int
+        the amount of material consumed per job
+
+    Returns
+    -------
+    str
+        the amount of remaining material expressed with its unit (e.g., "10kg").
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+total_material = 50000
+material_units = "kg"
+num_jobs = 200
+job_consumption = 100
+
+A = num_jobs * job_consumption
+B = (total_material - A)
+print("the amount of remaining material is", B, material_units)
+
+
+# In[5]:
+
+
+def interest(principal, rate, periods):
+    '''Interest.
+    3 points.
+
+    This function calculates the final value of an investment after
+        gaining simple interest over a number of periods.
+
+    To calculate simple interest, simply multiply the principal to the quantity (rate * time). 
+        Add this amount to the principal to get the final value.
+
+    Round down the final amount.
+
+    Parameters
+    ----------
+    principal: int
+        the principal (i.e., starting) amount invested, expressed in centavos
+    rate: float
+        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)
+    periods: int
+        the number of periods invested
+
+    Returns
+    -------
+    int
+        the final value of the investment
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+principal = 1000000
+rate = 0.20
+periods = 10
+
+A = rate * periods
+B = principal * A
+C = B + principal
+
+final_value = int(C)
+print("the final value of the investment is", final_value)
+
+
+# In[6]:
+
+
+def body_mass_index(weight, height):
+    '''Body Mass Index.
+    3 points.
+
+    This function calculates the body mass index (BMI) of a person
+        given their weight and height.
+
+    The formula for BMI is: kg / (m ^ 2)
+        (i.e., kilograms over meters squared)
+
+    Unfortunately, the users of this function use the imperial system.
+        You will need to first convert their arguments to the metric system.
+    
+    Parameters
+    ----------
+    weight: float
+        the weight of the person, in pounds
+    height: list
+        the height of the person, expressed as a list of two integers.
+        the first integer is the foot component of their height.
+        the second integer is the inches component of their height.
+        for example, 5'10" would be passed as [5, 10].
+
+    Returns
+    -------
+    float
+        the BMI of the person.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+weight = 123.2
+height = [5, 4]
+
+A = weight * 0.453592
+B = (height[0] * 0.3048) + (height[1] * 0.0254)
+print("BMI is", A / (B ** 2))
+
+
+# In[7]:
+
+
+def savings(gross_pay, tax_rate, expenses):
+    A = gross_pay - (gross_pay * tax_rate)
+    B = A - expenses
+    take_home_pay = int(B)
+    return int(B)
+
+
+# In[8]:
+
+
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    A = num_jobs * job_consumption
+    B = (total_material - A)
+    print(B, material_units)
+    return print(B, material_units)
+
+
+# In[9]:
+
+
+def interest(principal, rate, periods):
+    A = rate * periods
+    B = principal * A
+    C = B + principal
+    final_value = int(C)
+    return int(C)
+
+
+# In[10]:
+
+
+def body_mass_index(weight, height):
+    A = weight * 0.453592
+    B = (height[0] * 0.3048) + (height[1] * 0.0254)
+    return print(A / (B ** 2))
+
+
+# In[ ]:
+
+
+
+
